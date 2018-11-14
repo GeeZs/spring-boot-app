8e0e47b33d97e9aa7da34ae76d2e910bd19e25c3
Satan <GeeZs@users.noreply.github.com>
2018-11-14 17:59:50 +0200

    registration bugs fixed

--- src/main/java/com/cheshire/domain/User.java
+++ src/main/java/com/cheshire/domain/User.java
@@ -18,9 +18,6 @@ public class User implements UserDetails{
     private String username;
     @NotBlank(message = "Password cant't be empty")
     private String password;
-    @Transient
-    @NotBlank(message = "Password confirmation cant't be empty")
-    private String password2;
     private boolean active;
 
     private String email;
@@ -114,13 +111,5 @@ public class User implements UserDetails{
 
     public void setActivationCode(String activationCode) {
         this.activationCode = activationCode;
-    }
-
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
     }
 }
